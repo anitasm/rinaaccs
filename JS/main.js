eeb0b2b00f1fdb1842059987ad5ef9380c436258
anasolmendoza <soy@unmail.real>
2025-09-09 13:02:01 -0300

    Js a index= bienvenida y carrito= descuento con transferencia

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+function mostrarBienvenida() {
+  alert('Bienvenid@ a Rina Coledani :)');
+  const email = prompt('Únete a nuestro newsletter y se el primero en enterarte de las novedades!!!');
+  console.log('Email ingresado:', email);
+}
+
+mostrarBienvenida();
