41fcbc4011b28f79ab5e93afe54d4d793e6d1d09
anasolmendoza <soy@unmail.real>
2025-09-27 22:49:24 -0300

    Elimine los js viejos y el carrito, edite productos, idea de carrito burbuja

--- JS/main.js
+++
@@ -1,7 +0,0 @@
-function mostrarBienvenida() {
-  alert('Bienvenid@ a Rina Coledani :)');
-  const email = prompt('Únete a nuestro newsletter y se el primero en enterarte de las novedades!!!');
-  console.log('Email ingresado:', email);
-}
-
-mostrarBienvenida();
